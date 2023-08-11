4d8297593d723a4b5538abbb769338b19cb61c43
abhi1988sharma <abhi1988.sharma@gmail.com>
2023-08-11 21:42:32 +0300

    pushing changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import About from './About';
 import './App.css';
+import Alert from './components/Alert';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#093574';
+      showAlert?.("Dark mode is enabled", "success"); // Optional chaining here
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert?.("Light mode is enabled", "success"); // Optional chaining here
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><Router>
+      <Navbar title="TextUtils" aboutText="About textUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+
+      <div className='container my-3'>
+        
+          <Routes>
+            <Route exact path='/about' element={<About mode={mode}/>} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word counter, Character counter, Lower case to upper case, upper case to lower case, remove extra spaces" mode={mode} />} />
+          </Routes>
+        
+      </div>
+      </Router>
+    </>
   );
 }
 
